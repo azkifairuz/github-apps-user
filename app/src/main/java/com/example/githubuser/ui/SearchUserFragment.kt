1c324908da5291eaff32001cdaf0126765be4929
azkifairuz <azkiajmal@gmail.com>
2023-09-20 16:14:27 +0700

    search user viewmodel

--- app/src/main/java/com/example/githubuser/ui/SearchUserFragment.kt
+++ app/src/main/java/com/example/githubuser/ui/SearchUserFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.githubuser.R
+import com.example.githubuser.adapter.SearchUserAdapter
+import com.example.githubuser.databinding.FragmentSearchUserBinding
 
 class SearchUserFragment : Fragment() {
+    private lateinit var searchRv: RecyclerView
+    private lateinit var searchUserAdapter: SearchUserAdapter
+    private lateinit var binding: FragmentSearchUserBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_user, container, false)
+        binding = FragmentSearchUserBinding.inflate(inflater,container,false)
+        searchRv = binding.rvUser
+        searchRv.layoutManager = LinearLayoutManager(context)
+        searchUserAdapter = SearchUserAdapter(requireContext(), emptyList())
+        searchRv.adapter = searchUserAdapter
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/githubuser/viewmodel/SearchUserViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.githubuser.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.githubuser.data.response.ItemsItem
+
+class SearchUserViewModel:ViewModel() {
+    
+    private var _searchText = MutableLiveData<String>()
+    val searchText:LiveData<String> = _searchText
+
+    private var _isLoading = MutableLiveData<Boolean>()
+    val isLoading:LiveData<Boolean> = _isLoading
+
+    private var _listUser = MutableLiveData<List<ItemsItem>>()
+    val listUser:LiveData<List<ItemsItem>> = _listUser
+
+}
