419771e85e249d940cad5634b8ca2b70e305bfa4
azkifairuz <azkiajmal@gmail.com>
2023-09-20 15:07:00 +0700

    search user adapter

---
+++ app/src/main/java/com/example/githubuser/adapter/SearchUserAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.githubuser.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.githubuser.data.response.ItemsItem
+import com.example.githubuser.databinding.ItemUserBinding
+
+class SearchUserAdapter(private val context: Context, private var listUser: List<ItemsItem>) :
+    RecyclerView.Adapter<SearchUserAdapter.ListViewHolder>() {
+    class ListViewHolder(itemView: ItemUserBinding) : RecyclerView.ViewHolder(itemView.root) {
+        val imageProfil = itemView.profileImage
+        val fullName = itemView.fullName
+        val linkProfile = itemView.linkUrl
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemUserBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ListViewHolder(binding)
+
+    }
+
+    override fun getItemCount(): Int = listUser.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.imageProfil.load(
+            listUser[position].avatarUrl
+        )
+        holder.fullName.text = listUser[position].login
+        holder.linkProfile.text = listUser[position].htmlUrl
+        holder.linkProfile.setOnClickListener {
+            context.startActivity(
+                Intent
+                    (Intent.ACTION_VIEW, Uri.parse(holder.linkProfile.text.toString()))
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/githubuser/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/githubuser/data/retrofit/ApiService.kt
@@ -5,11 +5,13 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.Call
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("user/{username}")
+    @GET("search/users")
     @Headers("Authorization: token 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")
     fun findUser(
-        @Path("username") username: String
+        @Path("username") username: String,
+        @Query("uname") uname:String,
     ): Call<SearchUserResponse>
 }
