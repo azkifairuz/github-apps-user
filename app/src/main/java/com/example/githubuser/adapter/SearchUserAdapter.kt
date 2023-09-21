02bb45c74ca115471bfbe5e4246b8f29046fd7b7
azkifairuz <azkiajmal@gmail.com>
2023-09-21 20:48:22 +0700

    feat: callback search adapter

--- app/src/main/java/com/example/githubuser/adapter/SearchUserAdapter.kt
+++ app/src/main/java/com/example/githubuser/adapter/SearchUserAdapter.kt
@@ -12,10 +12,17 @@ import com.example.githubuser.databinding.ItemUserBinding
 
 class SearchUserAdapter(private val context: Context, private var listUser: List<ItemsItem>) :
     RecyclerView.Adapter<SearchUserAdapter.ListViewHolder>() {
+    private lateinit var toDetailCallback: ToDetailCallback
+
+
+    fun setToDetailCallback(toDetailCallback: ToDetailCallback){
+        this.toDetailCallback = toDetailCallback
+    }
     class ListViewHolder(itemView: ItemUserBinding) : RecyclerView.ViewHolder(itemView.root) {
         val imageProfil = itemView.profileImage
         val fullName = itemView.fullName
         val linkProfile = itemView.linkUrl
+        val cardView = itemView.cardView
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -40,5 +47,12 @@ class SearchUserAdapter(private val context: Context, private var listUser: List
                     (Intent.ACTION_VIEW, Uri.parse(holder.linkProfile.text.toString()))
             )
         }
+        holder.cardView.setOnClickListener {
+            toDetailCallback.onItemClicked(listUser[position])
+        }
     }
+    interface ToDetailCallback {
+        fun onItemClicked(user:ItemsItem)
+    }
+
 }
