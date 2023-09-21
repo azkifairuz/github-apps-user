b594061a502c855daaeebc15703c1b298c5d2a1c
azkifairuz <azkiajmal@gmail.com>
2023-09-21 19:53:40 +0700

    feat: detail user viewmodel

--- app/src/main/java/com/example/githubuser/ui/DetailUserFragment.kt
+++ app/src/main/java/com/example/githubuser/ui/DetailUserFragment.kt
@@ -6,14 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.githubuser.R
-class DetailUserFragment : Fragment() {
+import com.example.githubuser.databinding.FragmentDetailUserBinding
 
+class DetailUserFragment : Fragment() {
+    private lateinit var binding:FragmentDetailUserBinding
+
+    companion object{
+        const val EXTRA_FULLNAME = "extra_fullname"
+        const val EXTRA_USERNAME = "extra_username"
+        const val EXTRA_FOLOWERS = "extra_followers"
+        const val EXTRA_FOLOWINGS = "extra_followings"
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_user, container, false)
+        binding = FragmentDetailUserBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/example/githubuser/viewmodel/DetailUserViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.githubuser.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.githubuser.data.response.DetailUserResponse
+import com.example.githubuser.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DetailUserViewModel:ViewModel() {
+    private var _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private var _detailUser = MutableLiveData<DetailUserResponse>()
+    val detailUser: LiveData<DetailUserResponse> = _detailUser
+
+    fun getDetailuser(username:String){
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getDetailUser(username)
+        client.enqueue(object : Callback<DetailUserResponse> {
+            override fun onResponse(
+                call: Call<DetailUserResponse>,
+                response: Response<DetailUserResponse>
+            ) {
+                _isLoading.value = false
+                if (!response.isSuccessful){
+                    Log.e("isFailed Get User", " ${response.message()}")
+                    return
+                }
+                _detailUser.value = response.body()
+            }
+
+            override fun onFailure(call: Call<DetailUserResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e("isFailed Get User", " ${t.message.toString()}")
+            }
+
+        })
+    }
+}
