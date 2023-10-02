716072eb6a81ae640a37149d81d1ea096daa02d2
azkifairuz <azkiajmal@gmail.com>
2023-10-02 21:49:18 +0700

    feat:splahs screen

--- app/src/main/java/com/example/githubuser/ui/SplashScreen.kt
+++ app/src/main/java/com/example/githubuser/ui/SplashScreen.kt
@@ -1,12 +1,21 @@
 package com.example.githubuser.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.githubuser.MainActivity
 import com.example.githubuser.R
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000L)
     }
 }
